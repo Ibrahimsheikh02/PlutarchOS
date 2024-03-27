f7a158509633e8d28ae9812c972e73a191eacbfe
Ibrahimsheikh02 <mis9351@nyu.edu>
2024-03-27 04:00:42 -0400

    updated

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
+web: gunicorn lecturesgpt.wsgi:application --log-file -
 worker: python manage.py rqworker
-web: daphne lecturesgpt.asgi:application --port $PORT --bind 0.0.0.0 -v2 --http-timeout 30
--- base/routing.py
+++ base/routing.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import consumers
 from django.urls import re_path
 
-print ("Calling routing")
 websocket_urlpatterns = [
     re_path(r'^ws/chat/(?P<lecture_id>\d+)/$', consumers.YourStreamConsumer.as_asgi()),
 ]
-print ("Called routing")
